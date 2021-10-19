cb54eb9b55ce47ccb5d8a74c07d9ed5a2f7e9dae
Karina Andriushchenko <karina.andr@icloud.com>
2021-10-19 16:58:04 +0600

    #1 Added UserName validation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -107,5 +107,10 @@ namespace HeadHunter.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+        [AcceptVerbs("GET", "POST")]
+        public bool CheckName(string name)
+        {
+            return !_userManager.Users.Any(b => b.UserName == name);
+        }
     }
 }
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -15,6 +16,7 @@ namespace HeadHunter.ViewModels
 
         [Required]
         [Display(Name = "Nickname")]
+        [Remote(action: "CheckNickName", controller: "Account", ErrorMessage = "Это имя пользователя уже занято")]
         public string Nickname { get; set; }
 
         [Required]
