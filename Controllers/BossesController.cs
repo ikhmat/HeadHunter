9cfa87cbd2cf9362f84677e3153c5e206497e8ab
Abdilamitov Ikhmatillo <pro.ihmat@mail.ru>
2021-10-19 20:40:40 +0600

    #4 Added edit modal

---
+++ Controllers/BossesController.cs
@@ -0,0 +1,48 @@
+﻿using HeadHunter.Models;
+using HeadHunter.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeadHunter.Controllers
+{
+    public class BossesController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private ApplicationContext _db;
+
+        public BossesController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            BossesProfileViewModel viewModel = new BossesProfileViewModel
+            {
+                User = _userManager.Users.FirstOrDefault(u => u.Id == _userManager.GetUserId(User))
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(BossesProfileViewModel viewmodel)
+        {
+            User user = _db.Users.FirstOrDefault(u => u.UserName == viewmodel.User.UserName);
+            return RedirectToAction("Profile");
+        }
+
+    }
+}
---
+++ ViewModels/BossesProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HeadHunter.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeadHunter.ViewModels
+{
+    public class BossesProfileViewModel
+    {
+        public User User;
+        public IFormFile File;
+        public string UserName;
+    }
+}
