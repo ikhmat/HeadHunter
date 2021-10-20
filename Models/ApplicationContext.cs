76ca089f1d69c6bd70bf8622c11b426107644f8b
Abdilamitov Ikhmatillo <pro.ihmat@mail.ru>
2021-10-20 16:15:26 +0600

    #4 Added Vacancy, CategoryVacancy models

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -9,6 +9,11 @@ namespace HeadHunter.Models
 {
     public class ApplicationContext : IdentityDbContext<User>
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vacancy> Vacancies { get; set; }
+        public DbSet<CategoryVacancy> CategoryVacancies { get; set; }
+
+
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
     }
 }
---
+++ Models/Vacancy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeadHunter.Models
+{
+    public class Vacancy
+    {
+        public string Id { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Wage")]
+        public int Wage { get; set; }
+        public string Description { get; set; }
+        public int ExperienceFrom { get; set; }
+        public int ExperienceTo { get; set; }
+        [Required]
+        [Display(Name = "Category")]
+        public string CategoryVacancyId { get; set; }
+        public DateTime DateOfUpdate { get; set; }
+    }
+}
